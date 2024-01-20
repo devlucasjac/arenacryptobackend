84d942c6326349eeafefebf5a6c91cbf6f19fba3
lucas de souza jacinto <lucasdesouzajacinto@gmail.com>
2024-01-20 15:40:18 -0300

    rotas de captura de projetos por nome e categoria inseridas

--- blog/views.py
+++ blog/views.py
@@ -10,6 +10,23 @@ class CategoriaViewset(viewsets.ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
 
+    @action(detail=True,methods=["GET"])
+    def posts(self,request,pk=None):    
+        posts=Post.objects.filter(category_id=pk)
+        serializer = PostSerializer(posts,many=True)
+        return Response(serializer.data)
+
 class PostViewset(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    def list(self,request):
+        title = self.request.query_params.get('title')        
+        if title != None:            
+            posts = Post.objects.filter(titulo=title)
+            serializer = PostSerializer(posts,many=True)
+            return Response(serializer.data)
+        post =Post.objects.all() 
+        serializer = PostSerializer(post,many=True)
+        return Response(serializer.data)
+       
